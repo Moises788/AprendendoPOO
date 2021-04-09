93a65cfd3ef9a2ed55d24bf71b1046c0795dce5c
Moises788 <moisesmedeiros23@gmail.com>
2021-04-09 19:09:39 -0300

    Área de um triângulo

---
+++ AreaTriangulo/AreaTriangulo/AreaTriangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x;
+            x = new Triangulo();
+
+            Console.WriteLine("Indique os lados do triângulo x");
+            x.L1 = double.Parse(Console.ReadLine());
+            x.L2 = double.Parse(Console.ReadLine());
+            x.L3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área do triângulo x é: " + x.Area());
+        }
+    }
+}
