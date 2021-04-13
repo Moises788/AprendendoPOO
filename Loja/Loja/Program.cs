b349d706783dffe161da343b26d6fd660b810ea0
Moises788 <moisesmedeiros23@gmail.com>
2021-04-13 14:34:52 -0300

    Loja

---
+++ Loja/Loja/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            Console.WriteLine("Entre com os dados do produto");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preco: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Produto p = new(nome, preco);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine("quanto quer adicionar?" );
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine("quanto quer subtrair?");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados do produto: " + p);
+
+        }
+    }
+}
