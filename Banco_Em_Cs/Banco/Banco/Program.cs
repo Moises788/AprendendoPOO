043cd823542e8f90f0776e0f9e12a96e01ea7d1a
Moises788 <moisesmedeiros23@gmail.com>
2021-06-06 17:54:07 -0300

    banco adicionado

---
+++ Banco_Em_Cs/Banco/Banco/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco {
+    class Program {
+        static void Main(string[] args) {
+            Conta conta;
+
+            Console.WriteLine("Indique o seu nome:");
+            string titular = Console.ReadLine();
+
+            Console.WriteLine("Indique o seu número de usuário:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Deseja fazer saldo?");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 'n' || resp == 'N') {
+                conta = new Conta(numero, titular);
+            }
+            else {
+                Console.WriteLine("Informe o valor a ser depositado");
+                double saldo = double.Parse(Console.ReadLine());
+                conta = new Conta(numero, titular, saldo);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Entre com o valor do deposito:");
+            conta.Deposito(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("Entre com o valor de saque:");
+            conta.Saque(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+        }
+
+    }
+}
