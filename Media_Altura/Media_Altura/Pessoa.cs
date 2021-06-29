19fa2558b75e6ed2b0172d6ff95b38cf2cf90bce
Moises788 <moisesmedeiros23@gmail.com>
2021-06-29 16:34:09 -0300

    Adicionando programa que faz media da altura de pessoas

---
+++ Media_Altura/Media_Altura/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace Media_Altura {
+    class Pessoa {
+        public double Altura { get; set; }
+
+        public Pessoa(double altura) {
+            Altura = altura;
+        }
+    }
+}
