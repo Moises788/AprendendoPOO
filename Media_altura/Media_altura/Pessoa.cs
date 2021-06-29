88db8e3e1b60bfe6e14be7b22391bd6783906af2
Moises788 <moisesmedeiros23@gmail.com>
2021-06-29 16:26:44 -0300

    Programa que faz media das alturas

---
+++ Media_altura/Media_altura/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace Media_produto {
+    class Pessoa {
+        public double Altura { get; set; }
+        
+        public Pessoa(double altura) {
+            Altura = altura;
+        }
+    }
+}
---
+++ Media_altura/Media_altura/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Media_produto{
+    class Program {
+        static void Main(string[] args) {
+            double sum = 0;
+            double media = 0;
+
+            Console.WriteLine("Quantas pessoas serão medidas? ");
+            int n = int.Parse(Console.ReadLine());
+            Pessoa[] quantidade_pessoa = new Pessoa[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Digite a altura de cada pessoa: ");
+                quantidade_pessoa[i] = new Pessoa(double.Parse(Console.ReadLine()));
+            }
+            
+            for (int i = 0; i < n; i++) {
+                sum += quantidade_pessoa[i].Altura;
+            }
+            media = sum / n;
+            Console.WriteLine("A média é:" + media);
+        }
+    }
+}
